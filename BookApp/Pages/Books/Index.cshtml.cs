6675f6d68f7fabc3c98701e551561139ee46d729
KarolR <iggyfuntime666@gmail.com>
2021-05-14 14:59:07 +0200

    finished sorting

--- BookApp/Pages/Books/Index.cshtml.cs
+++ BookApp/Pages/Books/Index.cshtml.cs
@@ -20,9 +20,20 @@ namespace BookApp.Pages.Books
         }
 
         public IList<IndexDisplayModel> Books { get;set; }
-
-        public async Task OnGetAsync()
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string PublisherSort { get; set; }
+        public string GenreSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public async Task OnGetAsync(string sortOrder)
         {
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = sortOrder == "Author" ? "author_desc" : "Author";
+            PublisherSort = sortOrder == "Publisher" ? "publisher_desc" : "Publisher";
+            GenreSort = sortOrder == "Genre" ? "genre_desc" : "Genre";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
             IQueryable<IndexDisplayModel> BookIQ = _context.Books
                   .Select(p => new IndexDisplayModel
                   {
@@ -35,6 +46,41 @@ namespace BookApp.Pages.Books
                       DatePublished=p.DatePublished,
                       
                   });
+            switch(sortOrder)
+            {
+                case "title_desc":
+                    BookIQ = BookIQ.OrderByDescending(b => b.Title);
+                        break;
+                case "publisher_desc":
+                    BookIQ = BookIQ.OrderByDescending(b => b.PublisherName);
+                    break;
+                case "Publisher":
+                    BookIQ = BookIQ.OrderBy(b => b.PublisherName);
+                    break;
+                case "genre_desc":
+                    BookIQ = BookIQ.OrderByDescending(b => b.Genre);
+                    break;
+                case "Genre":
+                    BookIQ = BookIQ.OrderBy(b => b.Genre);
+                    break;
+                case "date_desc":
+                    BookIQ = BookIQ.OrderByDescending(b => b.DatePublished);
+                    break;
+                case "Date":
+                    BookIQ = BookIQ.OrderBy(b => b.DatePublished);
+                    break;
+                case "author_desc":
+                    BookIQ = BookIQ.OrderByDescending(b => b.AuthorFullName);
+                    break;
+                case "Author":
+                    BookIQ = BookIQ.OrderBy(b => b.AuthorFullName);
+                    break;
+                default:
+                    BookIQ = BookIQ.OrderBy(b => b.Title);
+                    break;
+
+
+            }
             Books = await BookIQ.AsNoTracking().ToListAsync();
         }
     }
